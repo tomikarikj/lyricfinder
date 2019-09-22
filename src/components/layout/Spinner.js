e2a5641f50e0159f2d12eb7105d5d06753179bd6
Tomi Karikj <karikjtomi@gmail.com>
2019-09-22 21:30:04 +0200

    Created and added styles for the spinner

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div
+      className="lds-ring"
+      style={{ width: '75px', margin: '60px auto', display: 'block' }}
+    >
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
