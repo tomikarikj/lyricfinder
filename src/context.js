4499848d966f2cab056ffa218692e3d634dae794
Tomi Karikj <karikjtomi@gmail.com>
2019-09-22 21:30:50 +0200

    Created the context provider

---
+++ src/context.js
@@ -0,0 +1,34 @@
+import React, { Component, createContext } from 'react';
+import axios from 'axios';
+
+const Context = createContext();
+
+export class Provider extends Component {
+  state = {
+    track_list: [],
+    heading: 'Top 10 tracks'
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => {
+        this.setState({ track_list: res.data.message.body.track_list });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <Context.Provider value={this.state}>
+          {this.props.children}
+        </Context.Provider>
+      </div>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
